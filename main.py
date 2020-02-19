d5e1ce6c5dd4331cd31f6390f6627f81f6862143
Zizou <auridor@yandex.ru>
2020-02-19 19:26:10 +0300

    first commit(correct file)

--- main.py
+++ main.py
@@ -1,25 +1,69 @@
-n = int(input())
-data = {
-    'янв': [],
-    'фев': [],
-    'мар': [],
-    'апр': [],
-    'май': [],
-    'июн': [],
-    "июл": [],
-    "авг": [],
-    "сен": [],
-    "окт": [],
-    "ноя": [],
-    "дек": []
+import os
+import sys
+from io import BytesIO
+# Этот класс поможет нам сделать картинку из потока байт
+import pygame
+import requests
+from PIL import Image
+
+pygame.init()
+
+
+def load_image(name, colorkey=None):
+    """
+    Function for loading pictures from Images folders
+    :param name: name of image which need to load
+    :param colorkey: background color
+    :return: loaded image
+    """
+    fullname = os.path.join('Images', name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+toponym_to_find = input()
+
+
+def get_request(toponym):
+    geocoder_api_server = "http://geocode-maps.yandex.ru/1.x/"
+
+    geocoder_params = {
+        "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+        "geocode": toponym,
+        "format": "json"}
+
+    response = requests.get(geocoder_api_server, params=geocoder_params)
+
+    if not response:
+        print('Error:', response.text)
+
+    return response.json()
+
+
+toponym = get_request(toponym_to_find)["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+toponym_coodrinates = toponym["Point"]["pos"]
+toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
+
+delta = "0.01"
+
+# Собираем параметры для запроса к StaticMapsAPI:
+map_params = {
+    "ll": ",".join([toponym_longitude, toponym_lattitude]),
+    "spn": ",".join([delta, delta]),
+    "l": "map"
 }
-for z in range(n):
-    s = input().split()
-    if s[2] in data:
-        data[s[2]].append(s[0])
-q = int(input())
-for z in range(q):
-    s = input()
-    month_bithd = data[s]
-    month_bithd.sort()
-    print(' '.join(month_bithd))
+
+map_api_server = "http://static-maps.yandex.ru/1.x/"
+# ... и выполняем запрос
+response = requests.get(map_api_server, params=map_params)
+
+Image.open(BytesIO(
+    response.content)).show()
+# Создадим картинку
+# и тут же ее покажем встроенным просмотрщиком операционной системы
