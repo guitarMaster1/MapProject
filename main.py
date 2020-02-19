eeeddd6f3a8277e67988e6bdb9ef99befe14e05e
Kuzzne4eK <mr_alex2003@mail.ru>
2020-02-19 20:07:13 +0300

    task 1

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from io import BytesIO
 import pygame
 import requests
 from PIL import Image
+import pygame
 
 pygame.init()
 
@@ -27,7 +28,7 @@ def load_image(name, colorkey=None):
     return image
 
 
-toponym_to_find = input()
+toponym_to_find = 'Москва Троицк В 38'
 
 
 def get_request(toponym):
@@ -46,7 +47,9 @@ def get_request(toponym):
     return response.json()
 
 
-toponym = get_request(toponym_to_find)["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+toponym = \
+    get_request(toponym_to_find)["response"]["GeoObjectCollection"]["featureMember"][0][
+        "GeoObject"]
 toponym_coodrinates = toponym["Point"]["pos"]
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
@@ -58,12 +61,40 @@ map_params = {
     "spn": ",".join([delta, delta]),
     "l": "map"
 }
-
 map_api_server = "http://static-maps.yandex.ru/1.x/"
 # ... и выполняем запрос
 response = requests.get(map_api_server, params=map_params)
+img = Image.open(BytesIO(response.content))
+img1 = img.save('1.png')
 
-Image.open(BytesIO(
-    response.content)).show()
-# Создадим картинку
-# и тут же ее покажем встроенным просмотрщиком операционной системы
+pygame.init()
+# размеры окна:
+size = width, height = (1200, 800)
+# screen — холст, на котором нужно рисовать:
+screen = pygame.display.set_mode(size)
+w = pygame.Color('white')
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join(name)
+    image = pygame.image.load(fullname).convert()
+    if colorkey is not None:
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+def draw():
+    screen.fill((0, 0, 0))
+    screen.blit(pygame.transform.scale(load_image('1.png'), (width, height)), (0, 0))
+
+
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+# завершение работы:
+pygame.quit()
