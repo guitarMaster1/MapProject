7ed2dd48b42b4cbc960df7c19376f689ff6d24a3
Svoboda21 <zotova_2003@bk.ru>
2020-02-23 09:26:11 +0300

    Add map scale

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 import os
 import sys
 from io import BytesIO
+
 import pygame
 import requests
 from PIL import Image
-import pygame
 
 pygame.init()
 
-WIDTH, HEIGHT = 600, 400
+WIDTH, HEIGHT = 650, 450
 
-
-toponym_to_find = 'Москва Троицк В 38'
+toponym_to_find = 'Москва Троицк Сиреневый 15'
 
 
 def get_request(toponym):
@@ -36,19 +35,22 @@ toponym = \
 toponym_coodrinates = toponym["Point"]["pos"]
 toponym_longitude, toponym_lattitude = toponym_coodrinates.split(" ")
 
-delta = "0.01"
+delta = "0.02"
 
 # Собираем параметры для запроса к StaticMapsAPI:
 map_params = {
     "ll": ",".join([toponym_longitude, toponym_lattitude]),
-    "spn": ",".join([delta, delta]),
-    "l": "map"
+    "l": "map",
+    "z": 14
 }
-map_api_server = "http://static-maps.yandex.ru/1.x/"
-# ... и выполняем запрос
-response = requests.get(map_api_server, params=map_params)
-img = Image.open(BytesIO(response.content))
-img1 = img.save('1.png')
+
+
+def send_request(params):
+    map_api_server = "http://static-maps.yandex.ru/1.x/"
+    response = requests.get(map_api_server, params=params)
+    img = Image.open(BytesIO(response.content))
+    img.save('1.png')
+
 
 pygame.init()
 # размеры окна:
@@ -75,20 +77,24 @@ def draw():
     screen.blit(pygame.transform.scale(load_image('1.png'), (WIDTH, HEIGHT)), (0, 0))
 
 
+send_request(map_params)
 draw()
 
-
 while pygame.event.wait().type != pygame.QUIT:
-    pygame.display.flip()
-
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_PAGEUP]:
-                print('Yes')
-            if keys[pygame.K_PAGEDOWN]:
-                print('No')
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_PAGEUP]:
+            send_request(map_params)
+            print('send')
+            map_params['z'] -= 1 if map_params['z'] > 1 else 0
+        if keys[pygame.K_PAGEDOWN]:
+            send_request(map_params)
+            print('send')
+            map_params['z'] += 1
+        draw()
+        pygame.display.flip()
 pygame.quit()
